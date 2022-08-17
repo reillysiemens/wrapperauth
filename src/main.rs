58f1b0ed780a24f2e16ee2da7ad89aaaa6980597
Reilly Tucker Siemens <reilly@tuckersiemens.com>
2022-08-16 23:04:46 -0700

    Extract flags into a common Target struct

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,39 @@
 use clap::Parser;
 
+#[derive(Parser)]
+struct Target {
+    /// Client ID.
+    #[clap(long)]
+    client: String,
+    /// Tenant ID.
+    #[clap(long)]
+    tenant: String,
+    /// Requested scopes.
+    #[clap(long, required = true)]
+    scopes: Vec<String>,
+}
+
 /// Do the good auth.
 #[derive(Parser)]
 #[clap(version)]
 enum Args {
     /// Acquire a token.
-    Auth {
-        /// Client ID.
-        #[clap(long)]
-        client: String,
-        /// Tenant ID.
-        #[clap(long)]
-        tenant: String,
-        /// Requested scopes.
-        #[clap(long, required = true)]
-        scopes: Vec<String>,
-    },
+    Auth(Target),
     /// Clear a token.
-    Clear {
-        /// Client ID.
-        #[clap(long)]
-        client: String,
-        /// Tenant ID.
-        #[clap(long)]
-        tenant: String,
-        /// Requested scopes.
-        #[clap(long, required = true)]
-        scopes: Vec<String>,
-    },
+    Clear(Target),
 }
 
 fn main() {
     match Args::parse() {
-        Args::Auth {
+        Args::Auth(Target {
             client,
             tenant,
             scopes,
-        } => println!("Acquired a token for {client} in {tenant} with {scopes:?}."),
-        Args::Clear {
+        }) => println!("Acquired a token for {client} in {tenant} with {scopes:?}."),
+        Args::Clear(Target {
             client,
             tenant,
             scopes,
-        } => println!("Cleared a token for {client} in {tenant} with {scopes:?}."),
+        }) => println!("Cleared a token for {client} in {tenant} with {scopes:?}."),
     }
 }
